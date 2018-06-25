5352ef3e826dc0c0ae827baa5257f28bbb5e29d0
Florian Kempenich <Flori@nKempenich.com>
2018-06-25 16:59:56 +0100

    Implement Formatter

--- shoppinglist/cost.py
+++ shoppinglist/cost.py
@@ -1,3 +1,7 @@
+import collections
+import textwrap
+
+
 class InvalidItem(Exception):
     pass
 
@@ -9,6 +13,7 @@ class CostCalculator:
 
     Warning: NOT Thread Safe!
     """
+
     def __init__(self, prices):
         self.prices = prices
 
@@ -17,10 +22,10 @@ class CostCalculator:
         self._ensure_valid_items()
 
         total = 0
-        rooms = {}
+        rooms = []
         for room in items_per_room:
             room_total = self._calculate_room_total(room)
-            rooms[room] = room_total
+            rooms.append((room, room_total))
             total += room_total
 
         return {
@@ -35,9 +40,47 @@ class CostCalculator:
             total += price
         return total
 
-
     def _ensure_valid_items(self):
         for room in self.items_per_room:
             for item in self.items_per_room[room]:
                 if item not in self.prices:
-                    raise InvalidItem("No price found for this item: '" + item + "'")
+                    raise InvalidItem(
+                        "No price found for this item: '" + item + "'")
+
+
+class CostFormatter:
+    def format(self, cost_result):
+        self.cost_result = cost_result
+
+        header_title = self._format_header_title()
+        rooms = self._format_rooms()
+        footer_total = self._format_footer_total()
+
+        return header_title + rooms + footer_total
+
+    def _format_header_title(self):
+        return textwrap.dedent("""
+            Total cost breakdown
+
+            ---""")
+
+    def _format_rooms(self):
+        formatted_rooms = ['']
+        for (room, cost) in self.cost_result['rooms']:
+            formatted_room_name = self._format_room_name(room)
+            formatted_rooms.append(f"{formatted_room_name}: {cost} €")
+
+        return '\n'.join(formatted_rooms)
+
+    @staticmethod
+    def _format_room_name(room_name):
+        return ' '.join(list(map(
+            lambda n: n.capitalize(),
+            room_name.split('-'))))
+
+    def _format_footer_total(self):
+        return textwrap.dedent(f"""
+            ---
+
+            Total: {self.cost_result['total']} €
+            """)
--- shoppinglist/test_cost.py
+++ shoppinglist/test_cost.py
@@ -1,6 +1,6 @@
+import textwrap
 import pytest
-from shoppinglist.cost import CostCalculator, InvalidItem
-
+from shoppinglist.cost import CostCalculator, InvalidItem, CostFormatter
 
 PRICES = {
     'InWallSwitch': 5,
@@ -9,80 +9,134 @@ PRICES = {
 }
 
 
-@pytest.fixture
-def calculator():
-    calculator = CostCalculator(PRICES)
-    return calculator
+class TestCalculator:
+    @pytest.fixture
+    def calculator(self):
+        calculator = CostCalculator(PRICES)
+        return calculator
 
+    def test_invalid_item(self, calculator):
+        with pytest.raises(InvalidItem) as e:
+            calculator.cost({
+                'kitchen': [
+                    'ThisItemHasNoPrice'
+                ]
+            })
 
-def test_invalid_item(calculator):
-    with pytest.raises(InvalidItem) as e:
-        calculator.cost({
+        assert str(
+            e.value) == "No price found for this item: 'ThisItemHasNoPrice'"
+
+    def test_single_room(self, calculator):
+        items_per_room = {
             'kitchen': [
-                'ThisItemHasNoPrice'
+                'InWallSwitch',
+                'BulbBasic'
             ]
-        })
-
-    assert str(e.value) == "No price found for this item: 'ThisItemHasNoPrice'"
-
-
-def test_single_room(calculator):
-    items_per_room = {
-        'kitchen': [
-            'InWallSwitch',
-            'BulbBasic'
-        ]
-    }
-
-    res = calculator.cost(items_per_room)
-
-    assert res == {
-        'total': 15,
-        'rooms': {
-            'kitchen': 15
         }
-    }
 
+        res = calculator.cost(items_per_room)
 
-def test_single_room_with_duplicates(calculator):
-    items_per_room = {
-        'kitchen': [
-            'InWallSwitch',
-            'InWallSwitch',
-            'InWallSwitch',
-            'InWallSwitch',
-            'BulbBasic'
-        ]
-    }
-
-    res = calculator.cost(items_per_room)
-
-    assert res == {
-        'total': 30,
-        'rooms': {
-            'kitchen': 30
+        assert res == {
+            'total': 15,
+            'rooms': [
+                ('kitchen', 15)
+            ]
         }
-    }
 
-
-def test_multiple_rooms(calculator):
-    items_per_room = {
-        'kitchen': [
-            'InWallSwitch',
-            'BulbBasic'
-        ],
-        'living-room': [
-            'BulbColor',
-            'BulbBasic',
-        ]
-    }
-
-    res = calculator.cost(items_per_room)
-
-    assert res == {
-        'total': 80,
-        'rooms': {
-            'kitchen': 15,
-            'living-room': 65
+    def test_single_room_with_duplicates(self, calculator):
+        items_per_room = {
+            'kitchen': [
+                'InWallSwitch',
+                'InWallSwitch',
+                'InWallSwitch',
+                'InWallSwitch',
+                'BulbBasic'
+            ]
         }
-    }
+
+        res = calculator.cost(items_per_room)
+
+        assert res == {
+            'total': 30,
+            'rooms': [
+                ('kitchen', 30)
+            ]
+        }
+
+    def test_multiple_rooms(self, calculator):
+        items_per_room = {
+            'kitchen': [
+                'InWallSwitch',
+                'BulbBasic'
+            ],
+            'living-room': [
+                'BulbColor',
+                'BulbBasic',
+            ]
+        }
+
+        res = calculator.cost(items_per_room)
+
+        assert res == {
+            'total': 80,
+            'rooms': [
+                ('kitchen', 15),
+                ('living-room', 65)
+            ]
+        }
+
+
+class TestFormatter:
+    @pytest.fixture
+    def formatter(self):
+        return CostFormatter()
+
+    def test_single_room(self, formatter):
+        assert formatter.format({
+            'total': 15,
+            'rooms': [
+                ('kitchen', 15),
+            ]
+        }) == textwrap.dedent("""
+            Total cost breakdown
+
+            ---
+            Kitchen: 15 €
+            ---
+
+            Total: 15 €
+            """)
+
+    def test_single_room_multiple_words(self, formatter):
+        assert formatter.format({
+            'total': 80,
+            'rooms': [
+                ('living-room', 80)
+            ]
+        }) == textwrap.dedent("""
+            Total cost breakdown
+
+            ---
+            Living Room: 80 €
+            ---
+
+            Total: 80 €
+            """)
+
+    def test_multiple_rooms(self, formatter):
+        assert formatter.format({
+            'total': 80,
+            'rooms': [
+                ('kitchen', 15),
+                ('living-room', 65)
+            ]
+        }) == textwrap.dedent("""
+            Total cost breakdown
+
+            ---
+            Kitchen: 15 €
+            Living Room: 65 €
+            ---
+
+            Total: 80 €
+            """)
